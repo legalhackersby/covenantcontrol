e00386cce28c42dcd16135f392dbef718a5c6e5c
dzmitry.lahoda <dzmitry.lahoda@protonmail.com>
2018-12-06 20:24:57 +0300

    Added dummy task list

---
+++ integration/ExtractorTest.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace integration
+{
+    public class ExtractorTest
+    {
+        [Fact]
+        public async Task Word()
+        {
+            
+        }     
+    }
+}
+
