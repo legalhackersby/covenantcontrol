59e0adf7d33c413b7a4a71d087f23da4072d81a4
dzmitry.lahoda <dzmitry.lahoda@protonmail.com>
2018-12-06 20:10:12 +0300

    tested local mongo and api

--- integration/MongoTest.cs
+++ integration/MongoTest.cs
@@ -2,15 +2,46 @@ using System;
 using Xunit;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace integration
 {
     public class MongoTest
     {
-        [Fact]
-        public void Test1()
+        public class Ping
         {
-          //MongoDB.Driver.MongoClient
+            public ObjectId Id { get; set; }
+            public DateTime Pong { get; set; }
+        }
+
+        [Fact]
+        public async Task Test1()
+        {
+            var client1 = new MongoClient();
+            var testDatabase = client1.GetDatabase("Test");
+            try
+            {
+                await testDatabase.CreateCollectionAsync(nameof(Ping));
+            }
+            catch (MongoDB.Driver.MongoCommandException ex)
+            {
+                if (ex.CodeName == "NamespaceExists" && ex.Code == 48)
+                {
+                    await testDatabase.DropCollectionAsync(nameof(Ping));
+                    await testDatabase.CreateCollectionAsync(nameof(Ping));
+                }
+                else throw;
+            }
+
+            var pings = testDatabase.GetCollection<Ping>(nameof(Ping), new MongoCollectionSettings { WriteConcern = WriteConcern.Acknowledged });
+            await pings.InsertOneAsync(new Ping { Pong = DateTime.UtcNow });
+
+            var client2 = new MongoClient();
+            var testDatabase2 = client2.GetDatabase("Test");
+            var pings2 = testDatabase2.GetCollection<Ping>(nameof(Ping), new MongoCollectionSettings { WriteConcern = WriteConcern.Acknowledged });
+            var result = await pings2.FindAsync(FilterDefinition<Ping>.Empty);
+            Assert.True(await result.AnyAsync());
         }
     }
 }
