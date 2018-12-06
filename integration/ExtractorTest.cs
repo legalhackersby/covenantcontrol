2477287b37009470a174284f483a50b23ddfa150
dzmitry.lahoda <dzmitry.lahoda@protonmail.com>
2018-12-06 20:36:17 +0300

    added sub for usage

--- integration/ExtractorTest.cs
+++ integration/ExtractorTest.cs
@@ -10,7 +10,10 @@ namespace integration
         [Fact]
         public async Task Word()
         {
-            
+            // TODO: add git lfs to test data
+            // TODO: run relative of with portable office?
+            // C:\Program Files\LibreOffice\program>python.exe F:\shared-src\chaintrack\src\Converter\unoconv\unoconv -f txt F:\shared-src\chaintrack\data\ping.docx
+
         }     
     }
 }
