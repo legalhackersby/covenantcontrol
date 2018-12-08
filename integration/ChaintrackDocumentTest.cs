f4901866c0f70d7a01c85c2c8dcfcaffa260f62b
dzmitry.lahoda <dzmitry.lahoda@protonmail.com>
2018-12-08 12:09:26 +0300

    added api to get body of doc

---
+++ integration/ChaintrackDocumentTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace integration
+{
+    public class ChaintrackDocumentTest
+    {
+        [Fact]
+        public async Task GetDocument()
+        {
+            var api = "api/document/acf26d66aa6143cbb138503f91063781";
+            var httpClient = new HttpClient();
+            var result = await httpClient.GetStringAsync(Settings.Host + api);
+            Assert.Contains(@"<mark className={'highlight'}>", result);
+            Assert.Contains(@"</mark>", result);
+        }
+    }
+}
+
