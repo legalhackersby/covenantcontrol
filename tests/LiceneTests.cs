0e437298225777e5ae912e5eff2ab919563ee1b0
dzmitry.lahoda <dzmitry.lahoda@protonmail.com>
2018-12-09 06:34:30 +0300

    added lucene to test

---
+++ tests/LiceneTests.cs
@@ -0,0 +1,17 @@
+
+using Xunit;
+using Lucene.Net;
+using Lucene.Net.Search;
+using Lucene.Net.Analysis;
+
+namespace tests
+{
+    public class LiceneTests
+    {
+        [Fact]
+        public void Stemming()
+        {
+         
+        }
+    }
+}
