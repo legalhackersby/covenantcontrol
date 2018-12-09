4a924c9966ca8903fed254489a5bb3cb48fd9000
dzmitry.lahoda <dzmitry.lahoda@protonmail.com>
2018-12-09 08:14:02 +0300

    added in ram index case

--- tests/LuceneTests.cs
+++ tests/LuceneTests.cs
@@ -8,21 +8,44 @@ using Lucene.Net.Store;
 using Lucene.Net.Search.Spans;
 using Lucene.Net.Documents;
 using Lucene.Net.Codecs;
+using Lucene.Net.Analysis.Query;
+using Lucene.Net.Analysis.Hunspell;
 using Lucene.Net.Search.Similarities;
 using Lucene.Net.Analysis.Standard;
 using Lucene.Net.Util;
+using System;
 
 namespace tests
 {
     public class LiceneTests
     {
         [Fact]
-        public void Stemming()
+        public void MultiDistanceAndFuzzyMatchSplitByParagraph()
         {
             var analyzer = new StandardAnalyzer(LuceneVersion.LUCENE_CURRENT);
-            var ramDirectory = new RAMDirectory();
-            var index = new IndexWriterConfig(LuceneVersion.LUCENE_CURRENT, analyzer);
+            var directory = new RAMDirectory();
+            var config = new IndexWriterConfig(LuceneVersion.LUCENE_CURRENT, analyzer);
+            var index = new IndexWriter(directory, config);
 
+            var documentText = "1.5. Договор вступает в силу с даты подписания сторонами. Срок действия договора устанавливается до 31.08.2019 года.";
+            var covenant = "Срок действия договора устанавливается до 31.08.2019 года.";
+            var keywords = new []{"Договор", "вступает", "в силу"};
+            var document = new Document();
+            document.AddTextField("paragraph", documentText, Field.Store.YES); 
+            index.AddDocument(document);
+            index.Dispose();
+           // var queryParser = new FuzzyQuery()
+            var reader = DirectoryReader.Open(directory);
+            var searcher = new IndexSearcher(reader);
+            var regexResults = searcher.Search(new RegexpQuery(new Term("keyword", "Договр")), 1);
+            var scoredDocs = regexResults.ScoreDocs;
+            //Assert.True(scoredDocs.Length > 0);
+            foreach(var doc in scoredDocs)
+            {
+                var data = searcher.Doc(doc.Doc);
+                var found = data.Get("paragraph");
+                Console.WriteLine(found);
+            }
         }
     }
 }
