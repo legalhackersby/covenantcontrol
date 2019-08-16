95401a1c5e27671da8f8655be7d11b0cb9406d9f
RuslanShpileuski <ruslan.shpileuski@gmail.com>
2019-08-16 14:19:55 +0300

    fixing covenant regex tests

--- tests/RegexTests.cs
+++ tests/RegexTests.cs
@@ -17,6 +17,8 @@ namespace tests
         /// </summary>
         private readonly ITextParserService textParserService;
 
+        private string TestDataPath => Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, "data", "tests");
+
         /// <summary>
         /// Initializes a new instance of the <see cref="RegexTests"/> class.
         /// </summary>
@@ -31,25 +33,27 @@ namespace tests
         [Fact]
         public void Contract1_GetCovenantResults()
         {
+            var path = Path.Combine(TestDataPath, "1.txt");
+            var text = File.ReadAllText(path);
+
             var textParserService1 = new TextParserService(new ExactMatchCovenantSearchStrategy());
-            var result1 = textParserService1.GetCovenantResults(ContractTextHelper.Contract1);
+            var result1 = textParserService1.GetCovenantResults(text);
 
             var extParserService2 = new TextParserService(new WordsPercentageMatchCovenantSearchStrategy());
-            var nextResult = extParserService2.GetCovenantResults(ContractTextHelper.Contract1);
+            var nextResult = extParserService2.GetCovenantResults(text);
 
             var result3 = result1.Select(_ => _.StartIndex).Except(nextResult.Select(_ => _.StartIndex)).ToList();
 
             // exact first covenant in documenst
             var covenantsInOrder = nextResult.OrderBy(x => x.StartIndex).ToArray();
+
             // discuss why we include or exclude some issues
             var covenant1 = "1.5. Договор вступает в силу с даты подписания сторонами. Срок действия договора устанавливается до 31.08.2019 года.\r";
-            var covenant2 = "2.2. Арендная плата по п. 2.1. настоящего договора производится Арендатором ежемесячно, начиная с даты подписания Акта приема-передачи, в срок до 15 (пятнадцатого) числа текущего месяца. Арендатор предоставляет Арендодателю в срок до 20 (двадцатого) числа отчетного месяца копию платежного поручения о перечислении суммы арендной платы по адресу: Республика Беларусь, город Минск, ул. Радужная, 25. Копия платежного поручения должна содержать отметку обслуживающего банка о проведении платежа.\r";
-            var covenant3 = "2.3. Размер арендной платы, указанный в п.2.1. настоящего договора, может изменяться в одностороннем порядке по инициативе Арендодателя, но не чаще одного раза в год. О предстоящем изменении размера арендной платы Арендатор извещается Арендодателем не позднее, чем за 30 календарных дней.\r";
-            var covenant6 = "3.2.8. Не менее одного раза в 3 года производить за свой счет текущий ремонт в арендуемом Помещении, а также осуществлять ремонт фасада здания и прилегающей территории по согласованию с Арендодателем.\r";
+            var covenant2 = "2. РАЗМЕР АРЕНДНОЙ ПЛАТЫ И ПОРЯДОК РАСЧЕТОВ\r";
+            var covenant3 = "2.1. Сумма ежемесячной арендной платы составляет 2 000 (две тысячи)  российских рублей без НДС.. НДС уплачивается в порядке, установленном международными договорами, участниками которых являются Российская Федерация и Республика Беларусь. Расчет стоимости арендной платы прилагается (Приложение №5).\r";
             Assert.Equal(covenant1, covenantsInOrder[0].CovenantValue);
             Assert.Equal(covenant2, covenantsInOrder[1].CovenantValue);
             Assert.Equal(covenant3, covenantsInOrder[2].CovenantValue);
-            //Assert.Equal(16, result.Count);
             //Assert.True(result.All(_ => _.CovenantValue.Length > 3));
             //Assert.True(result.All(_ => _.StartIndex < _.EndIndex));
             //Assert.True(result.Distinct().Count() == result.Count);
@@ -61,16 +65,18 @@ namespace tests
         [Fact]
         public void Contract2_GetCovenantResults()
         {
-            var result = this.textParserService.GetCovenantResults(ContractTextHelper.Contract2);
-            Assert.Equal(25, result.Count);
+            var path = Path.Combine(TestDataPath, "2.txt");
+            var text = File.ReadAllText(path);
+
+            var result = this.textParserService.GetCovenantResults(text);
+            Assert.Equal(93, result.Count);
             Assert.True(result.All(_ => _.CovenantValue.Length > 3));
             Assert.True(result.All(_ => _.StartIndex < _.EndIndex));
             Assert.True(result.Distinct().Count() == result.Count);
 
             // exact first covenant in document
             var covenantsInOrder = result.OrderBy(x => x.StartIndex).ToArray();
-            var covenant1 = "Срок действия договора устанавливается до 31.08.2019 года.";
-            Assert.Equal(covenant1, covenantsInOrder[0].CovenantValue);
+            Assert.Contains("«Индекс» – Европейский Средний Общий индекс", covenantsInOrder[0].CovenantValue);
         }
 
         /// <summary>
@@ -79,8 +85,11 @@ namespace tests
         [Fact]
         public void Contract3_GetCovenantResults()
         {
-            var result = this.textParserService.GetCovenantResults(ContractTextHelper.Contract3);
-            Assert.Equal(17, result.Count);
+            var path = Path.Combine(TestDataPath, "3.txt");
+            var text = File.ReadAllText(path);
+
+            var result = this.textParserService.GetCovenantResults(text);
+            Assert.Equal(22, result.Count);
             Assert.True(result.All(_ => _.CovenantValue.Length > 3));
             Assert.True(result.All(_ => _.StartIndex < _.EndIndex));
             Assert.True(result.Distinct().Count() == result.Count);
@@ -92,9 +101,11 @@ namespace tests
         [Fact]
         public void Contract1_GetCovenantResults_Strategy()
         {
-            var text = ContractTextHelper.Contract1;
+            var path = Path.Combine(TestDataPath, "1.txt");
+            var text = File.ReadAllText(path);
+
             var textParserService1 = new TextParserService(new ExactMatchCovenantSearchStrategy());
-            var result = textParserService1.GetCovenantResults(ContractTextHelper.Contract1);
+            var result = textParserService1.GetCovenantResults(text);
 
             foreach (var covenantSearchResult in result)
             {
@@ -112,10 +123,10 @@ namespace tests
                 sw.Flush();
             }
 
-            var text1 = ContractTextHelper.Contract1;
+            var text1 = File.ReadAllText(path);
 
             var textParserService2 = new TextParserService(new WordsPercentageMatchCovenantSearchStrategy());
-            var result2 = textParserService2.GetCovenantResults(ContractTextHelper.Contract1);
+            var result2 = textParserService2.GetCovenantResults(text1);
 
             foreach (var covenantSearchResult in result2)
             {
@@ -144,10 +155,13 @@ namespace tests
         [Fact]
         public void Contract1_GetCovenantResults_Strategy2()
         {
+            var path = Path.Combine(TestDataPath, "1.txt");
+            var text = File.ReadAllText(path);
+
             var text1 = @"Sentence 1 Test. Sentence 2. Sentence 3 Test. Sentence 4 Test. Sentence 5. \n";
 
             var textParserService2 = new TextParserService(new PresizeWordsPercentageMathCovenantSearchStrategy());
-            var result2 = textParserService2.GetCovenantResults(ContractTextHelper.Contract1);
+            var result2 = textParserService2.GetCovenantResults(text);
 
             foreach (var covenantSearchResult in result2)
             {
